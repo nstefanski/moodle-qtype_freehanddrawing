c9b5a171a38511fc0dda32fd272f182da2d64d39
Jacob Shapiro <jacob.shapiro@let.ethz.ch>
2013-05-22 18:01:16 +0200

    Changed placement of form elements in edit page.

--- edit_canvas_form.php
+++ edit_canvas_form.php
@@ -90,39 +90,41 @@ class qtype_canvas_edit_form extends question_edit_form {
         
         
         
-        $mform->addElement('header', 'qtype_canvas_drawing_parameters', get_string('drawing_parameters', 'qtype_canvas'));
-        $mform->addElement('select', 'radius',
-                get_string('radius', 'qtype_canvas'), array(
-                0 => 1,
-                1 => 3,
-                2 => 5,
-                3 => 7,
-                4 => 9,
-                5 => 11,
-                6 => 13,
-                7 => 15,
-                8 => 17,
-                9 => 19));
-
-       $mform->addElement('select', 'threshold',
-                get_string('threshold', 'qtype_canvas'), array(
-                0 => 50,
-                1 => 55,
-                2 => 60,
-                3 => 65,
-                4 => 70,
-                5 => 75,
-                6 => 80,
-                7 => 85,
-                8 => 90,
-                9 => 95,
-                10 => 100));
+        $mform->addElement('header', 'qtype_canvas_drawing_background_image', get_string('drawing_background_image', 'qtype_canvas'));
+  
  
 
         // TODO: Implement this: http://docs.moodle.org/dev/Using_the_File_API_in_Moodle_forms#Load_existing_files_into_draft_area
         $mform->addElement('filepicker', 'qtype_canvas_image_file', get_string('file'), null,
                            array('maxbytes' => 1572864/*1.5MB*/, 'accepted_types' => array('image', 'picture')));
+
         $mform->addElement('header', 'qtype_canvas_drawing', get_string('drawing', 'qtype_canvas'));
+        $mform->addElement('select', 'radius',
+        		get_string('radius', 'qtype_canvas'), array(
+        				0 => 1,
+        				1 => 3,
+        				2 => 5,
+        				3 => 7,
+        				4 => 9,
+        				5 => 11,
+        				6 => 13,
+        				7 => 15,
+        				8 => 17,
+        				9 => 19));
+        
+        $mform->addElement('select', 'threshold',
+        		get_string('threshold', 'qtype_canvas'), array(
+        				0 => 50,
+        				1 => 55,
+        				2 => 60,
+        				3 => 65,
+        				4 => 70,
+        				5 => 75,
+        				6 => 80,
+        				7 => 85,
+        				8 => 90,
+        				9 => 95,
+        				10 => 100));
         //$mform->closeHeaderBefore('drawsolution');
         //$mform->addElement('html', '<img ALT="Erase Canvas" SRC="'.$CFG->wwwroot . '/question/type/canvas/pix/Eraser-icon.png" CLASS="qtype_canvas_eraser" ID="qtype_canvas_eraser_id_0" '.$eraserHTMLParams.'>');
         $mform->addElement('textarea', 'qtype_canvas_textarea_id_0', get_string("drawingrawdata", "qtype_canvas"), 'class="qtype_canvas_textarea" wrap="virtual" rows="20" cols="50"');
