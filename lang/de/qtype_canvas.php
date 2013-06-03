c73777d62717e4ca0bf337c925a23751b20550e5
Jacob Shapiro <jacob.shapiro@let.ethz.ch>
2013-06-03 15:26:17 +0200

    Added German support (preliminary via Google Translate).

---
+++ lang/de/qtype_canvas.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_canvas', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage canvas
+ * @copyright  ETHZ LET <jacob.shapiro@let.ethz.ch>
+ * @license    http://opensource.org/licenses/BSD-3-Clause
+ */
+
+$string['pluginname'] = 'die Leinwand';
+$string['pluginname_help'] = 'In Antwort auf eine Frage der Befragten zieht eine Antwort auf eine vordefinierte Bild. Es gibt nur eine richtige Antwort möglich.';
+$string['pluginname_link'] = 'question/type/canvas';
+$string['pluginnameadding'] = 'Hinzufügen einer Leinwand Frage';
+$string['pluginnameediting'] = 'Bearbeiten einer Leinwand Frage';
+$string['pluginnamesummary'] = 'In Antwort auf eine Frage der Befragten zieht eine Antwort auf eine vordefinierte Bild. Es gibt nur eine richtige Antwort möglich.';
+$string['threshold_for_correct_answers'] = 'Threshold für richtige Antworten (%)';
+//$string['addmoreanswerblanks'] = 'Blanks for {no} More Answers';
+$string['answer'] = 'Antwort: {$a}';
+//$string['answermustbegiven'] = 'You must enter an answer if there is a grade or feedback.';
+//$string['answerno'] = 'Answer {$a}';
+$string['correctansweris'] = 'Die richtige Antwort lautet: {$a}.';
+//$string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
+$string['pleaseenterananswer'] = 'Bitte geben Sie eine Antwort.';
+$string['drawing_background_image'] = 'Hintergrund Bild';
+$string['drawingrawdata'] = '';
+$string['backgroundfilemustbegiven'] = 'Sie müssen eine Datei als Hintergrundbild für die Leinwand zu spezifizieren.';
+$string['drawingmustbegiven'] = 'Sie müssen eine Zeichnung.';
+$string['drawanswer'] = 'zeichnen beantworten';
+$string['drawing'] = 'Zeichnen';
+$string['accepted_background_image_file_types'] = 'Akzeptierte Dateitypen';
+$string['nobackgroundimageselectedyet'] = 'Kein Hintergrundbild ausgewählt.';
+$string['are_you_sure_you_want_to_erase_the_canvas'] = 'Sind Sie sicher, dass Sie die Leinwand zu löschen?';
+$string['are_you_sure_you_want_to_pick_a_new_bgimage'] = 'Sie haben etwas auf die Leinwand gezeichnet. Die Wahl eines neuen Image-Datei löscht diese. Sind Sie sicher, dass Sie weitermachen?';
+$string['are_you_sure_you_want_to_change_the_drawing_radius'] = 'Wenn Sie die Ziehradius jetzt ändern, werde ich die ganze Leinwand zu löschen. Sind Sie damit einverstanden?';
+$string['set_radius'] = 'Geben Radius von Punkten (Pixel)';
+$string['threshold_must_be_reasonable'] = 'Sie müssen eine angemessene Schwelle.';
+$string['erase_canvas'] = 'Lösch Leinwand';
+$string['no_response_summary'] = 'Keine Reaktion Zusammenfassung';
+$string['no_correct_answer_summary'] = 'Keine richtige Antwort Zusammenfassung Leinwand Fragetypen.';
+$string['out_of_necessary'] = 'von notwendigen';
