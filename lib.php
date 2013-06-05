67c9e5a8d55d80284d96ece17a181898ca4489e7
Jacob Shapiro <jacob.shapiro@let.ethz.ch>
2013-06-05 15:50:36 +0200

    Clean up lib.

--- lib.php
+++ lib.php
@@ -34,6 +34,5 @@ defined('MOODLE_INTERNAL') || die();
 function qtype_canvas_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
     global $CFG;
     require_once($CFG->libdir . '/questionlib.php');
-    question_pluginfile($course, $context, 'qtype_canvas',
-                                                                $filearea, $args, $forcedownload, $options);
+    question_pluginfile($course, $context, 'qtype_canvas', $filearea, $args, $forcedownload, $options);
 }
