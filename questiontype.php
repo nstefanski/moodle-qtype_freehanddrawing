123cb49cd80f579be0068ad135fafba0ccd54277
Jacob Shapiro <jacob.shapiro@let.ethz.ch>
2014-03-19 11:47:31 +0100

    import / export works now from question bank xml files still needs work on course import / export

--- questiontype.php
+++ questiontype.php
@@ -77,7 +77,18 @@ class qtype_freehanddrawing extends question_type {
 		// Save the new answer:
         $answer = new stdClass();
         $answer->question = $question->id;
-        $answer->answer = $question->qtype_freehanddrawing_textarea_id_0;
+        if (array_key_exists('qtype_freehanddrawing_textarea_id_0', $question) === true) {
+            // we are called from a web-form saving us
+            $answer->answer = $question->qtype_freehanddrawing_textarea_id_0;
+        } else {
+            // we are called from an import procedure
+            if (count($question->answer) >= 1) {
+                $answer->answer = $question->answer[0];
+            } else {
+                // no answers available...
+                return false;
+            }
+        }
         $answer->feedback = '';
         $answer->id = $DB->insert_record('question_answers', $answer);
         
@@ -130,12 +141,103 @@ class qtype_freehanddrawing extends question_type {
     }    
     
 
- 
-    
-    
+    function export_to_xml($question, qformat_xml $format, $extra=null) {
+    	$extraquestionfields = $this->extra_question_fields();
+    	if (!is_array($extraquestionfields)) {
+    		return false;
+    	}
+    	//omit table name (question)
+    	array_shift($extraquestionfields);
+    	$expout='';
+
+    	foreach ($extraquestionfields as $field) {
+    		$exportedvalue = $question->options->$field;
+    		if (!empty($exportedvalue) && htmlspecialchars($exportedvalue) != $exportedvalue) {
+    			$exportedvalue = '<![CDATA[' . $exportedvalue . ']]>';
+    		}
+    		$expout .= "    <$field>{$exportedvalue}</$field>\n";
+    	}
+
+    	$expout .= '<bgimage>';
+    	$bgImageArray = qtype_freehanddrawing_renderer::get_image_for_question($question);
+    	$expout .= '<filename>' . $bgImageArray[3] .  '</filename>';
+    	$expout .= '<dataURL><![CDATA[' . $bgImageArray[0] . ']]></dataURL>';
+    	$expout .= '</bgimage>';
 
 
-    
-    
+    	foreach ($question->options->answers as $answer) {
+    		$percent = 100 * $answer->fraction;
+    		$expout .= "    <answer fraction=\"$percent\">\n";
+    		$expout .= $format->writetext($answer->answer, 3, false);
+    		$expout .= "      <feedback format=\"html\">\n";
+    		$expout .= $format->writetext($answer->feedback, 4, false);
+    		$expout .= "      </feedback>\n";
+    		$expout .= "    </answer>\n";
+    	}
+    	return $expout;
+    }
+    public function import_from_xml($data, $question, qformat_xml $format, $extra=null) {
+    	if (!isset($data['@']['type']) || $data['@']['type'] != 'freehanddrawing') {
+    		return false;
+    	}
+
+    	$question = $format->import_headers($data);
+    	$question->qtype = 'freehanddrawing';
+
+    	$question->shuffleanswers = array_key_exists('shuffleanswers', $format->getpath($data, array('#'), array()));
+
+    	$format->import_combined_feedback($question, $data, true);
+    	$format->import_hints($question, $data, true, false, $format->get_format($question->questiontextformat));
+
+    	// Save Extra Fields ---------------
+    	$extraquestionfields = $this->extra_question_fields();
+    	if (!is_array($extraquestionfields)) {
+    		return false;
+    	}
+    	//omit table name (question)
+    	array_shift($extraquestionfields);
+    	$question->options = new stdClass();
+    	foreach ($extraquestionfields as $field) {
+    		$question->$field = $format->getpath($data, array('#', $field, '0', '#'), 'does_not_exist');
+    		if ($question->$field === 'does_not_exist') {
+    			return false;
+    		}
+    	}
+    	// Save drawn solution -------------------------------------
+    	$question->qtype_freehanddrawing_textarea_id_0 = $format->getpath($data, array('#', 'answer', '0', '#', 'text', '0', '#'), 'does_not_exist');
+    	if ($question->qtype_freehanddrawing_textarea_id_0 === 'does_not_exist') {
+    		return false;
+    	}
+    	// Save canvas background image file ---------------------
+    	$bgImageArray[0] = $format->getpath($data, array('#', 'bgimage', '0', '#', 'dataURL', '0', '#'), 'does_not_exist');
+    	$bgImageArray[3] = $format->getpath($data, array('#', 'bgimage', '0', '#', 'filename', '0', '#'), 'does_not_exist');
+
+    	if ($bgImageArray[0] === 'does_not_exist' || $bgImageArray[3] === 'does_not_exist') {
+    		return false;
+    	}
+    	// Convert dataURL to binary
+    	$imgBinaryData = base64_decode(qtype_freehanddrawing_renderer::strstr_after($bgImageArray[0], 'base64,'));
+    	// Make sure this is a valid image file we could read
+    	if (($GDimg = imagecreatefromstring($imgBinaryData)) === false) {
+    		return false;
+    	}
+    	// Clean up GD resource
+    	imagedestroy($GDimg);
+    	// Prepare draft file area which would later be really saved in ::save_question_options() when the question object already exists
+    	global $USER;
+    	$fs = get_file_storage();
+    	$usercontext = context_user::instance($USER->id);
+    	$question->qtype_freehanddrawing_image_file = file_get_unused_draft_itemid();
+    	$record = new stdClass();
+    	$record->contextid = $usercontext->id;
+    	$record->component = 'user';
+    	$record->filearea  = 'draft';
+    	$record->itemid    = $question->qtype_freehanddrawing_image_file;
+    	$record->filename  = $bgImageArray[3];
+    	$record->filepath  = '/';
+    	$fs->create_file_from_string($record, $imgBinaryData);
+
+    	return $question;
+    }
     
 }
